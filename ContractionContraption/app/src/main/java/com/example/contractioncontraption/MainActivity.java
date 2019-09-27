51509e032ea664714fc7c7c4c0d26e6d867548e1
NonlinearFruit <1123benji5813@gmail.com>
2019-09-27 06:41:38 -0500

    push button, show interval lengths = minimal viable product

--- ContractionContraption/app/src/main/java/com/example/contractioncontraption/MainActivity.java
+++ ContractionContraption/app/src/main/java/com/example/contractioncontraption/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.contractioncontraption;
 
 import android.os.Bundle;
+
+import com.example.contractioncontraption.core.AllDatLogic;
+import com.example.contractioncontraption.core.CoreLogic;
+import com.example.contractioncontraption.core.TimeWrapper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private AllDatLogic logic;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +31,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        logic = new CoreLogic(new TimeWrapper());
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 );
+        listView.setAdapter(adapter);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar
+                        .make(view, "Contraction Started!", Snackbar.LENGTH_SHORT)
+                        .show();
+                logic.startContraction();
+                printIntervals();
             }
         });
+    }
+
+    private void printIntervals() {
+        adapter.clear();
+        List<Long> intervals = logic.getContrcationIntervals();
+        for (long interval : intervals) {
+           long minutes = Math.round(interval / 60);
+           String message = minutes + " minutes";
+           adapter.add(message);
+        }
     }
 
     @Override
