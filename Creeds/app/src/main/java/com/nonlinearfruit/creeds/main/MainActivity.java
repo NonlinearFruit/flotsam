c5319074b8208b5f6e5bdc2644c5ba71543285e0
NonlinearFruit <1123benji5813@gmail.com>
2020-04-22 15:39:14 -0500

    Add preference options!

--- Creeds/app/src/main/java/com/nonlinearfruit/creeds/main/MainActivity.java
+++ Creeds/app/src/main/java/com/nonlinearfruit/creeds/main/MainActivity.java
@@ -1,10 +1,17 @@
 package com.nonlinearfruit.creeds.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -12,30 +19,54 @@ import android.widget.SearchView;
 
 import com.nonlinearfruit.creeds.R;
 import com.nonlinearfruit.creeds.main.models.MainMenuItem;
+import com.nonlinearfruit.creeds.preferences.PreferenceActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-    private SearchView searchView;
-    private ListView listView;
     private MainAdapter adapter;
-    private MainDatabase db;
+    private ListView listView;
+    private Boolean preferencesHaveChanged = false;
+    private SharedPreferences.OnSharedPreferenceChangeListener listener; // If reference isn't stored, it is garbage collected and lost
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        //preferences.edit().remove("creeds").commit();
         setContentView(R.layout.activity_list);
 
-        searchView = findViewById(R.id.search_view);
-        listView = findViewById(R.id.list_view);
-        db = new MainDatabase();
-        adapter = new MainAdapter(db.getMainMenuItems(), this);
-
         setupListView();
+        setupAdapter(listView);
         setupSearchView();
+        setupPreferenceListener();
+    }
+
+    private void setupPreferenceListener() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences preference, String newValue) {
+                Log.d("Creeds", "Preference changed");
+                preferencesHaveChanged = true;
+            }
+        };
+        preferences.registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    private void setupAdapter(ListView listView) {
+        adapter = new MainAdapter(getFilteredCreeds(), this);
+        listView.setAdapter(adapter);
     }
 
     private void setupListView() {
-        listView.setAdapter(adapter);
+        listView = findViewById(R.id.list_view);
         listView.setTextFilterEnabled(false);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,9 +81,65 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     }
 
     private void setupSearchView() {
+        SearchView searchView = findViewById(R.id.search_view);
         searchView.setIconifiedByDefault(false);
         searchView.setOnQueryTextListener(this);
         searchView.setSubmitButtonEnabled(false);
+    }
+
+    private List<MainMenuItem> getFilteredCreeds() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        MainDatabase db = new MainDatabase();
+        List<MainMenuItem> items = db.getMainMenuItems();
+
+        Set<String> defaultCheckedCreeds = new HashSet<>();
+        for (MainMenuItem item : items)
+           defaultCheckedCreeds.add(item.CreedTitle);
+        Set<String> checkedCreeds = preferences.getStringSet("creeds", defaultCheckedCreeds);
+
+        List<MainMenuItem> filteredItems = new ArrayList<MainMenuItem>();
+        for (MainMenuItem item: items)
+            if (checkedCreeds.contains(item.CreedTitle))
+                filteredItems.add(item);
+        return sortList(filteredItems);
+    }
+
+    private List<MainMenuItem> sortList(List<MainMenuItem> items) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final Boolean chronologicalOrder = preferences.getBoolean("order", true);
+        Collections.sort(items, new Comparator<MainMenuItem>() {
+            @Override
+            public int compare(MainMenuItem a, MainMenuItem b) {
+                return chronologicalOrder
+                        ? b.CreedYear.compareTo(a.CreedYear)
+                        : b.CreedTitle.compareTo(a.CreedTitle);
+            }
+        });
+        return items;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("Creeds", "onResume");
+        if (!preferencesHaveChanged)
+            return;
+
+        setupAdapter(listView);
+        preferencesHaveChanged = true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        menu.add(0, 0, 0, "Preferences");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(MainActivity.this, PreferenceActivity.class);
+        this.startActivity(intent);
+        return true;
     }
 
     @Override
---
+++ Creeds/app/src/main/java/com/nonlinearfruit/creeds/preferences/PreferenceFragment.java
@@ -0,0 +1,65 @@
+package com.nonlinearfruit.creeds.preferences;
+
+import android.os.Bundle;
+
+import androidx.preference.MultiSelectListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.nonlinearfruit.creeds.R;
+import com.nonlinearfruit.creeds.main.MainDatabase;
+import com.nonlinearfruit.creeds.main.models.MainMenuItem;
+
+import java.util.List;
+import java.util.Set;
+
+public class PreferenceFragment extends PreferenceFragmentCompat {
+
+    private int numberOfCreeds = 0;
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preference_screen);
+        MultiSelectListPreference preferredCreeds = getPreferredCreeds();
+        setPreferredCreedsEntries(preferredCreeds);
+        setPreferredCreedsListener(preferredCreeds);
+        updatePreferredCreedsSummary(preferredCreeds);
+    }
+
+    private MultiSelectListPreference getPreferredCreeds() {
+        return (MultiSelectListPreference) findPreference("creeds");
+    }
+
+    private void setPreferredCreedsEntries(MultiSelectListPreference preferedCreeds) {
+        List<MainMenuItem> items = new MainDatabase().getMainMenuItems();
+        CharSequence[] entries = new CharSequence[items.size()];
+        for(int i = 0; i < items.size(); i++) {
+            MainMenuItem item = items.get(i);
+            entries[i] = item.CreedTitle;
+        }
+        preferedCreeds.setEntries(entries);
+        preferedCreeds.setEntryValues(entries);
+        numberOfCreeds = entries.length;
+    }
+
+    private void setPreferredCreedsListener(MultiSelectListPreference preferedCreeds) {
+        preferedCreeds.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        if (preference instanceof MultiSelectListPreference)
+                            updatePreferredCreedsSummary((MultiSelectListPreference) preference, (Set<String>) newValue);
+                        return true;
+                    }
+                }
+        );
+    }
+
+    private void updatePreferredCreedsSummary(MultiSelectListPreference preferedCreeds) {
+        updatePreferredCreedsSummary(preferedCreeds, preferedCreeds.getValues());
+    }
+
+    private void updatePreferredCreedsSummary(MultiSelectListPreference preferedCreeds, Set<String> values) {
+        preferedCreeds.setSummary(values.size()+" / "+numberOfCreeds);
+    }
+}
